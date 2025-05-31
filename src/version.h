286e2f7b3d56a3617cf2f1adf2d7841c7e86171b
remmirath90 <morphs90@gmail.com>
2025-05-31 13:34:03 +0200

    Bumped internal version to 0.4.0.3 for RC2

--- src/version.h
+++ src/version.h
@@ -43,7 +43,7 @@ const char *GetSuppliceVersion();
 /** Lots of different version numbers **/
 
 #define VERSIONSTR "4.12.2"
-#define SUPPVERSIONSTR "v0.4.0.2"
+#define SUPPVERSIONSTR "v0.4.0.3"
 
 // The version as seen in the Windows resource
 #define RC_FILEVERSION 4,14,2,0
